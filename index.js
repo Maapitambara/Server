2c8dc2cedfaddb9da42cc9f3e661ce1758b05c2b
Maapitambara <info.maapitamera@gmail.com>
2025-12-11 10:11:49 +0530

    Update index.js
    fix api call

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://clientsite-navy.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
@@ -176,3 +176,4 @@ app.delete("/api/images/:id", async (req, res) => {
 app.listen(process.env.PORT, () =>
   console.log(`🚀 Server running on http://localhost:${process.env.PORT}`)
 );
+
